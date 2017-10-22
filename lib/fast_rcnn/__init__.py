34a75ac751cbb957a14868704742dbec5954dbd0
zyclark <zyclark@gmail.com>
2017-10-22 20:36:43 +0200

    upload local files to github

---
+++ lib/fast_rcnn/__init__.py
@@ -0,0 +1,5 @@
+from . import config
+from . import train
+from . import test
+from . import nms_wrapper
+# from nms_wrapper import nms
---
+++ lib/rpn_msr/testAnchorTarget.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 21 11:57:21 2017
+
+@author: clark
+"""
+
+import numpy as np
+
+
+def _whctrs(anchor):
+    """
+    Return width, height, x center, and y center for an anchor (window).
+    """
+
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[1] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    """
+    Given a vector of widths (ws) and heights (hs) around a center
+    (x_ctr, y_ctr), output a set of anchors (windows).
+    """
+
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         y_ctr - 0.5 * (hs - 1),
+                         x_ctr + 0.5 * (ws - 1),
+                         y_ctr + 0.5 * (hs - 1)))
+    return anchors
+
+def _scale_enum(anchor, scales):
+    """
+    Enumerate a set of anchors for each scale wrt an anchor.
+    """
+
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    ws = w * scales
+    hs = h * scales
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+# _anchors = generate_anchors(scales=np.array(anchor_scales))
+#    _num_anchors = _anchors.shape[0]
+
+base_size=16
+base_anchor = np.array([1, 1, base_size, base_size]) - 1
+ratios=[0.5, 1, 2]
+#ratio_anchors = _ratio_enum(base_anchor, ratios)
+
+w, h, x_ctr, y_ctr = _whctrs(base_anchor)#0 0 15 15-> 16 16 7.5 7.5
+#use center of base anchor to generate 9 anchors
+size = w * h
+size_ratios = size / ratios#256/[0.5,1,2]->512 256 128
+ws = np.round(np.sqrt(size_ratios))#23 16 11
+hs = np.round(ws * ratios)#12 16 22 
+ratio_anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+scales=2**np.arange(2, 6)#4 8 16 32 
+re_anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
+                         for i in xrange(ratio_anchors.shape[0])])
+
+    
+width=14
+height=14
+_feat_stride=16
+shift_x = np.arange(0, width) * _feat_stride
+shift_y = np.arange(0, height) * _feat_stride
+shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+shifts = np.vstack((shift_x.ravel(), shift_y.ravel(),
+                        shift_x.ravel(), shift_y.ravel())).transpose()
+A =re_anchors.shape[0] 
+K = shifts.shape[0]
+anchors = re_anchors.reshape((1, A, 4)) + \
+              shifts.reshape((1, K, 4)).transpose((1, 0, 2))
+anchors = anchors.reshape((K * A, 4))
+#keep only inside
+all_anchors=anchors
+_allowed_border=0
+im_info=[800,800,1]
+inds_inside = np.where(
+        (all_anchors[:, 0] >= -_allowed_border) &
+        (all_anchors[:, 1] >= -_allowed_border) &
+        (all_anchors[:, 2] < im_info[1] + _allowed_border) &  # width
+        (all_anchors[:, 3] < im_info[0] + _allowed_border)    # height
+    )[0]
+
+in_anchors = all_anchors[inds_inside, :]
